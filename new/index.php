556c2a46384b1e7cf9d4fd4d8717a93b02fb1558
Miguel Lopez <tramitador20@gmail.com>
2020-04-12 03:33:37 -0400

    organize code

--- new/index.php
+++ new/index.php
@@ -368,49 +368,12 @@
   <script type="text/javascript" src="js/bootstrap.min.js"></script>
   <!-- MDB core JavaScript -->
   <script type="text/javascript" src="js/mdb.min.js"></script>
+  <!-- Main JavaScript -->
+  <script type="text/javascript" src="js/main.js"></script>
   <!-- Initializations -->
   <script type="text/javascript">
     // Animations initialization
     new WOW().init();
-
-    //nav bar active item
-    $(document).ready(function() {
-        $('.nav-item').click(function() {
-            $('.nav-item.active').removeClass("active");
-            $(this).addClass("active");
-        });
-    });
-
-
-    $(document).ready(function() {
-        var form = $('#contactForm'); // contact form
-        var submit = $('.submit-btn'); // submit button
-        var alert = $('.alert-msg'); // alert div for show alert message
-
-        // form submit event
-        form.on('submit', function(e) {
-            e.preventDefault(); // prevent default form submit
-
-            $.ajax({
-                url: 'mail.php', // form action url
-                type: 'POST', // form submit method get/post
-                dataType: 'html', // request type html/json/xml
-                data: form.serialize(), // serialize form data
-                beforeSend: function() {
-                    alert.fadeOut();
-                    submit.html('Enviando...'); // change submit button text
-                },
-                success: function(data) {
-                    alert.html(data).fadeIn(); // fade in response data
-                    form.trigger('reset'); // reset form
-                    submit.attr("style", "display: none !important");; // reset submit button text
-                },
-                error: function(e) {
-                    console.log(e)
-                }
-            });
-        });
-    });
   </script>
 </body>
 
