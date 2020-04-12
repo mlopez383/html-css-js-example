6652c28e94f914466b98d2bcd3f0c9a7e77a33d6
Miguel Lopez <tramitador20@gmail.com>
2020-04-12 02:52:22 -0400

    Change video

---
+++ new/index.php
@@ -0,0 +1,377 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Material Design Bootstrap</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body class="grey lighten-3">
+
+  <!--Main Navigation-->
+  <header>
+
+    <!-- Navbar -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light white scrolling-navbar">
+      <div class="container">
+
+        <!-- Brand -->
+        <a class="navbar-brand waves-effect" href="#">
+          <strong class="red-text font-weight-bold">JesucristoSalvacion.com</strong>
+        </a>
+
+        <!-- Collapse -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Links -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+          <!-- Left -->
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link waves-effect" href="#">Inicio
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link waves-effect" href="#nosotros">Nosotros</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link waves-effect" href="#contacto">Contacto</a>
+            </li>
+          </ul>
+
+          <!-- Right -->
+          <ul class="navbar-nav nav-flex-icons">
+            <li class="nav-item">
+              <a href="https://www.facebook.com/jesucristo.salvacion.7982" class="nav-link waves-effect" target="_blank">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.youtube.com/watch?v=52PKyR4nyOA" class="nav-link waves-effect" target="_blank">
+                <i class="fab fa-youtube"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Navbar -->
+
+  </header>
+  <!--Main Navigation-->
+
+  <!--Main layout-->
+  <main class="mt-5 pt-2">
+    <div class="container">
+
+      <!--Section: Post-->
+      <section class="mt-4">
+
+        <!--Grid row-->
+        <div class="row">
+
+          <!--Grid column-->
+          <div class="col-md-8 mb-3">
+
+            <!--Card: Jumbotron-->
+            <div class="card red-passion-gradient mb-4 wow fadeIn">
+              <!-- Content -->
+              <div class="card-body text-white text-center">
+                <div class="white-text mb-3"><i class="fas fa-cross fa-4x"></i></div>
+                <h5 class="mb-3">
+                  <strong>EL PLAN DE SALVACIÓN</strong>
+                </h5>
+                <h1 class="mb-1">
+                  <strong>JESUCRISTO</strong>
+                </h1>
+                <p class="mb-0">
+                  <strong>Porque de tal manera amó Dios al mundo, que ha dado a su Hijo unigénito, 
+                    para que todo aquel que en él cree, no se pierda, mas tenga vida eterna.
+                    Juan 3:16
+                  </strong>
+                </p>
+              </div>
+              <!-- Content -->
+            </div>
+            <!--Card: Jumbotron-->
+
+            <div class="card mb-4 wow fadeIn">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/rrmtZJULq0U?rel=0" allowfullscreen></iframe>
+              </div>
+            </div>
+
+            <!--Card-->
+            <div class="card mb-4 wow fadeIn">
+              <span id="paso1" class="card-ancla"></span>
+              <!--Card content-->
+              <div class="card-body">
+                <p class="h3 my-1 text-center">EL PROBLEMA </p>
+                <p class="h6 text-center">TODOS ESTAMOS EN LA MISMA POSICIÓN: SEPARADOS DE DIOS</p>
+                <div class="mt-4"></div>
+                <span class="my-4 deep-orange-text font-weight-bolder font-italic">
+                  "Por cuanto todos pecaron, y están destituidos de la gloria de Dios." 
+                </span>
+                <span class="ml-2 my-4 deep-orange-text"> Romanos 3:23</span>
+                <div class="mt-2"></div>
+                <span class="my-4 deep-orange-text font-weight-bolder font-italic">
+                  "Porque la paga del pecado es muerte." 
+                </span>
+                <span class="ml-2 my-4 deep-orange-text"> Romanos 6:23a</span>
+                <div class="mt-2"></div>
+
+                <p class="text-justify">Dios nos creó para que podamos vivir con El para siempre pero a causa del pecado
+                  que heredeamos estamos separados de Dios. Como humanos pecamos sin querer o
+                  intencionalmente, y esto forma una nube entre nosotros y Dios. Cada uno de nosotros
+                  rompe la ley de Dios y es imposible que alcancemos sus estándares... somos pecadores
+                  naturaleza, por práctica y por elección.
+                </p>
+                <img src="img/el-problema.png" class="img-fluid" alt="">
+                <div class="mt-3"></div>
+                <p class="h6 text-center">MUCHOS HAN TRATADO DE UNIR
+                  LA SEPARACIÓN POR DIFERENTES MEDIOS
+                  PERO TODOS SIN EXITO
+                </p>
+              </div>
+            </div>
+            <!--/.Card-->
+
+            <!--Card-->
+            <div class="card mb-4 wow fadeIn">
+              <span id="paso2" class="card-ancla"></span>
+              <!--Card content-->
+              <div class="card-body">
+                <p class="h3 my-1 text-center">LA RESPUESTA DE DIOS </p>
+                <p class="h6 text-center">HAY UN SOLO CAMINO A DIOS</p>
+                <div class="mt-4"></div>
+                <img src="img/la-respuesta.png" class="img-fluid" alt="">
+                <div class="mt-4"></div>
+                <span class="my-4 deep-orange-text font-weight-bolder font-italic">
+                  "Porque por gracia sois salvos por medio de la fe; y esto no de vosotros, pues es don de Dios
+                  ; no por obras, para que nadie se gloríe" 
+                </span>
+                <span class="ml-2 my-4 deep-orange-text"> Efesios 2:8-9</span>
+                <div class="mt-2"></div>
+                <span class="my-4 deep-orange-text font-weight-bolder font-italic">
+                  "Mas Dios muestra su amor para con nosotros, en que siendo aún pecadores, Cristo murió or nosotros." 
+                </span>
+                <span class="ml-2 my-4 deep-orange-text"> Romanos 5:8</span>
+                <div class="mt-2"></div>
+                <span class="my-4 deep-orange-text font-weight-bolder font-italic">
+                  "Jesús le dijo: Yo soy el camino, y la verdad, y la vida; nadie viene al Padre, sino or mí." 
+                </span>
+                <span class="ml-2 my-4 deep-orange-text"> Juan 14:6</span>
+              </div>
+            </div>
+            <!--/.Card-->
+
+            <!--Card-->
+            <div class="card mb-4 wow fadeIn">
+              <span id="paso3" class="card-ancla"></span>
+              <!--Card content-->
+              <div class="card-body">
+                <p class="h3 my-1 text-center">QUE DEBO HACER AHORA</p>
+                <p class="h6 text-center">YO NO PUEDO CONSTRUIR EL PUENTE, PERO PUEDO
+                  ELEGIR CRUZAR POR EL QUE DIOS PROVEYÓ
+                </p>
+                <div class="mt-4"></div>
+                <div>
+                  <div>Hay dos pasos a seguir:</div>
+                  <div>
+                    <span>1)</span>
+                    <span class="font-weight-bolder">Arrenpentimiento... </span>
+                    <span class="deep-orange-text font-italic">"Arrepiéntete y vuelve hacia Dios, para que tus pecados sean perdonados"</span>
+                    <span class="deep-orange-text">Hechos 3:19</span>
+                  </div>
+                  <div>
+                    <span>2)</span>
+                    <span class="font-weight-bolder">Fe... </span>
+                    <span class="deep-orange-text font-italic">"Mas a todos los que le recibieron, a los que creen en su nombre, les dio potestad de ser hechos hijos de Dios"</span>
+                    <span class="deep-orange-text">Juan 1:12</span>
+                  </div>
+                </div>
+                <p class="text-justify">Si yo no cruzo el puente estoy separado de Dios para siempre -en el infierno-. Pero si realmente quiero
+                  cambiar mi dirección y confiar en Cristo, estaré con Dios para siempre -en el cielo-. ¿Hay alguna buena razón para que usted no haga esto ahora?.
+                </p>
+                <div class="font-weight-bolder">¿COMO?</div>
+                <div>-Admita que es un pecador: "soy pecador".</div>
+                <div>-Con la ayuda de Dios abandone el pecado.</div>
+                <div>-Crea que Cristo Jesús murió en la cruz y resucitó para salvarlo.</div>
+                <p class="text-justify">La siguiente breve oración puede servirle de ejemplo para entregarse al Salvador y Señor Jesucristo: </p>
+                <p class="text-justify deep-orange-text font-italic">"Señor Jesús, es verdad que soy un pecador y necesito de tu perdón. Creo
+                  que has muerto en mi lugar para pagar la pena que me corresponde por mis pecados y que resucitaste de entre los muertos. Vuelvo
+                  ahora mi vida hacia Tí y te pido que vivas en mi corazón como Señor y Salvador."
+                </p>
+                <p class="text-justify">Si usted realmente ha tomado esta desición (la más importante de su vida, ¡su destino eterno depende
+                  de ello!), busque una iglesia que estudie la Palabra de Dios, no busque una religión o una iglesia que simplemente esté cerca
+                  de su casa, es muy importante que la Biblia sea la base de las predicaciones y estudios.</p>
+              </div>
+            </div>
+            <!--/.Card-->
+
+            <!--Card-->
+            <div class="card mb-4 wow fadeIn">
+              <span id="nosotros" class="card-ancla"></span>
+              <div class="card-header font-weight-bold">Nosotros</div>
+              <!--Card content-->
+              <div class="card-body">
+                <p class="text-justify">Somos creyentes que quieren agradar y obedecer a Dios, preocupados por las almas 
+                  que todavía no fueron salvas.</p>
+                <span class="my-4 deep-orange-text font-weight-bolder font-italic">
+                  "Pero recibiréis poder, cuando haya venido sobre vosotros el Espíritu Santo, y me seréis testigos en Jerusalén, en toda Judea, en Samaria, y hasta lo último de la tierra." 
+                </span>
+                <span class="ml-2 my-4 deep-orange-text">Hechos 1:8</span>
+              </div>
+            </div>
+            <!--/.Card-->
+
+            <!--Reply-->
+            <div class="card mb-3 wow fadeIn">
+              <span id="contacto" class="card-ancla"></span>
+              <div class="card-header font-weight-bold">Contáctanos</div>
+              <div class="card-body">
+
+                <!-- Default form reply -->
+                <form>
+
+                  <!-- Comment -->
+                  <div class="form-group">
+                    <label for="replyFormComment">Tu comentario/mensaje</label>
+                    <textarea class="form-control" id="replyFormComment" rows="5"></textarea>
+                  </div>
+
+                  <!-- Name -->
+                  <label for="replyFormName">Tu nombre</label>
+                  <input type="email" id="replyFormName" class="form-control">
+
+                  <br>
+
+                  <!-- Email -->
+                  <label for="replyFormEmail">Tu e-mail</label>
+                  <input type="email" id="replyFormEmail" class="form-control">
+
+
+                  <div class="text-center mt-4">
+                    <button class="btn btn-info btn-md" type="submit">Enviar</button>
+                  </div>
+                </form>
+                <!-- Default form reply -->
+
+              </div>
+            </div>
+            <!--/.Reply-->
+
+          </div>
+          <!--Grid column-->
+         
+
+          <!--Grid column-->
+          <div class="col-md-4 mb-4">
+            <!--Card-->
+            <div class="card mb-4 wow fadeIn sticky-top sticky-offset">
+              <div class="card-header text-center font-weight-bold">EL PLAN</div>
+              <!--Card content-->
+              <div class="card-body">
+                <ul class="list-unstyled">
+                  <li class="media">
+                    <div class="media-body">
+                      <a href="#paso1">
+                        <h5 class="mt-0 mb-1 font-weight-bold deep-orange-text">I. El Problema</h5>
+                      </a>
+                    </div>
+                  </li>
+                  <li class="media my-4">
+                    <div class="media-body">
+                      <a href="#paso2">
+                        <h5 class="mt-0 mb-1 font-weight-bold deep-orange-text">II. La Respuesta de Dios</h5>
+                      </a>
+                    </div>
+                  </li>
+                  <li class="media">
+                    <div class="media-body">
+                      <a href="#paso3">
+                        <h5 class="mt-0 mb-1 font-weight-bold deep-orange-text">III. ¿Qué debo hacer ahora?</h5>
+                      </a>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <!--/.Card-->
+
+            <!--Card: -->
+            <div class="mb-4 wow fadeIn">
+                <img src="img/Jesus.png" class="img-fluid" alt="">
+            </div>
+            <!--Card: -->
+
+          </div>
+          <!--Grid column-->
+
+        </div>
+        <!--Grid row-->
+
+      </section>
+      <!--Section: Post-->
+
+    </div>
+  </main>
+  <!--Main layout-->
+
+  <!--Footer-->
+  <footer class="page-footer text-center font-small mdb-color darken-2 mt-4 wow fadeIn">
+
+    <hr class="my-4">
+
+    <!-- Social icons -->
+    <div class="pb-4">
+      <a href="https://www.facebook.com/jesucristo.salvacion.7982" target="_blank">
+        <i class="fab fa-facebook-f mr-3"></i>
+      </a>
+      <a href="https://www.youtube.com/watch?v=52PKyR4nyOA" target="_blank">
+        <i class="fab fa-youtube mr-3"></i>
+      </a>
+    </div>
+    <!-- Social icons -->
+
+    <!--Copyright-->
+    <div class="footer-copyright py-3">
+      © 2020 JesucristoSalvacion.com
+    </div>
+    <!--/.Copyright-->
+
+  </footer>
+  <!--/.Footer-->
+
+  <!-- SCRIPTS -->
+  <!-- JQuery -->
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <!-- Initializations -->
+  <script type="text/javascript">
+    // Animations initialization
+    new WOW().init();
+  </script>
+</body>
+
+</html>
